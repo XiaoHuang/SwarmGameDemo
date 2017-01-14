394238533e4d93edd2aec5b1157429092f72d6fb
XiaoHuang <magnificentxiao@gmail.com>
2017-01-14 12:09:27 -0800

    Add hero swarm
    Add hero swarm, and hero will leads swarm direction

---
+++ Assets/EnemySwarmBehavior.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+// An implementation of the flocking algorithm: http://www.red3d.com/cwr/boids/
+// Additional resources:
+// http://harry.me/2011/02/17/neat-algorithms---flocking/
+public class EnemySwarmBehavior : MonoBehaviour {
+	/// <summary>
+	/// the number of drones we want in this swarm
+	/// </summary>
+	public int droneCount = 200;
+	public float spawnRadius = 50f;
+	public List<GameObject> drones;
+
+	public Vector2 swarmBounds = new Vector2(300f, 300f);
+
+	public GameObject prefab;
+
+	// Use this for initialization
+	protected virtual void Start () {
+		if (prefab == null)
+		{
+			// end early
+			Debug.Log("Please assign a drone prefab.");
+			return;
+		}
+
+		// instantiate the drones
+		GameObject droneTemp;
+		drones = new List<GameObject>();
+		for (int i = 0; i < droneCount; i++)
+		{
+			prefab.tag = "EnemyDrone";
+			droneTemp = (GameObject) GameObject.Instantiate(prefab);
+			droneTemp.GetComponent<Renderer> ().material.color = Color.blue;
+
+			EnemyDroneBehavior db = droneTemp.GetComponent<EnemyDroneBehavior>();
+			Debug.Log (db);
+			db.drones = this.drones;
+			db.swarm = this;
+
+			// spawn inside circle
+			Vector2 pos = new Vector2(transform.position.x, transform.position.z) + Random.insideUnitCircle * spawnRadius;
+			droneTemp.transform.position = new Vector3(pos.x, transform.position.y, pos.y);
+			droneTemp.transform.parent = transform;
+
+			drones.Add(droneTemp);
+		}
+	}
+
+	// Update is called once per frame
+	protected virtual void Update () {
+
+	}
+
+	protected virtual void OnDrawGizmosSelected()
+	{
+		Gizmos.DrawWireCube(transform.position, new Vector3(swarmBounds.x, 0f, swarmBounds.y));
+		Gizmos.DrawWireSphere(transform.position, spawnRadius);
+	}
+}
---
+++ Assets/SwarmBehavior.cs
@@ -0,0 +1,86 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+// An implementation of the flocking algorithm: http://www.red3d.com/cwr/boids/
+// Additional resources:
+// http://harry.me/2011/02/17/neat-algorithms---flocking/
+public class SwarmBehavior : MonoBehaviour {
+	/// <summary>
+	/// the number of drones we want in this swarm
+	/// </summary>
+	public int droneCount = 200;
+	public float spawnRadius = 50f;
+	public List<GameObject> drones;
+	public GameObject droneHero;
+	public Vector2 swarmBounds = new Vector2(300f, 300f);
+
+	public GameObject prefab;
+	public GameObject heroPrefab;
+
+	// Use this for initialization
+	protected virtual void Start () {
+		if (prefab == null)
+		{
+			// end early
+			Debug.Log("Please assign a drone prefab.");
+			return;
+		}
+
+		// instantiate the drones
+		GameObject droneTemp;
+		drones = new List<GameObject>();
+		droneHero = new GameObject();
+
+		// create droneHero
+		heroPrefab.tag = "DroneHero";
+		droneTemp = (GameObject) GameObject.Instantiate(heroPrefab);
+		droneTemp.GetComponent<Renderer> ().material.color = Color.yellow;
+
+		DroneHeroBehavior dbHero = droneTemp.GetComponent<DroneHeroBehavior>();
+		Debug.Log (this.drones);
+		Debug.Log (dbHero);
+		dbHero.drones = this.drones;
+		dbHero.swarm = this;
+		dbHero.droneHero = droneTemp;
+		this.droneHero = droneTemp;
+
+		// spawn inside circle
+		Vector2 pos = new Vector2(transform.position.x, transform.position.z) + Random.insideUnitCircle * spawnRadius;
+		droneTemp.transform.position = new Vector3(pos.x, transform.position.y, pos.y);
+		droneTemp.transform.parent = transform;
+
+		drones.Add(droneTemp);
+
+		// create all drones;
+		for (int i = 1; i < droneCount; i++)
+		{
+			prefab.tag = "Drone";
+			droneTemp = (GameObject) GameObject.Instantiate(prefab);
+			droneTemp.GetComponent<Renderer> ().material.color = Color.red;
+
+			DroneBehavior db = droneTemp.GetComponent<DroneBehavior> ();	
+			db.drones = this.drones;
+			db.swarm = this;
+			db.droneHero = this.droneHero;
+
+			// spawn inside circle
+			pos = new Vector2(transform.position.x, transform.position.z) + Random.insideUnitCircle * spawnRadius;
+			droneTemp.transform.position = new Vector3(pos.x, transform.position.y, pos.y);
+			droneTemp.transform.parent = transform;
+			
+			drones.Add(droneTemp);
+		}
+	}
+	
+	// Update is called once per frame
+	protected virtual void Update () {
+	
+	}
+
+	protected virtual void OnDrawGizmosSelected()
+	{
+		Gizmos.DrawWireCube(transform.position, new Vector3(swarmBounds.x, 0f, swarmBounds.y));
+		Gizmos.DrawWireSphere(transform.position, spawnRadius);
+	}
+}
